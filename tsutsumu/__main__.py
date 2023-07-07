e813aacabd107f98f1d9bb8317a669854e75b0cc
Robert Grimm <apparebit@gmail.com>
2023-07-07 15:34:01 -0400

    Refactor code into functions that can be called from outside the module

--- tsutsumu/__main__.py
+++ tsutsumu/__main__.py
@@ -8,7 +8,7 @@ import traceback
 from .maker import BundleMaker
 
 
-if __name__ == '__main__':
+def parser() -> ArgumentParser:
     try:
         width = min(os.get_terminal_size()[0], 70)
     except:
@@ -16,15 +16,6 @@ if __name__ == '__main__':
 
     def width_limited_formatter(prog: str) -> HelpFormatter:
         return RawTextHelpFormatter(prog, width=width)
-
-    @dataclass
-    class ToolOptions:
-        bundle_only: bool = False
-        output: 'None | str' = None
-        package: 'None | str' = None
-        repackage: bool = False
-        verbose: bool = False
-        roots: 'list[str]' = field(default_factory=list)
 
     parser = ArgumentParser('tsutsumu',
         description=dedent("""
@@ -35,20 +26,29 @@ if __name__ == '__main__':
             tool skips directories and files that do not have valid Python
             module names.
 
-            By default, the bundle script executes a __main__ module just like
-            "python -m package" does. If the bundled modules include exactly one
-            such __main__ module, that module is automatically selected.
-            Otherwise, please use the -p/--package option to specify the package
-            name.
+            By default, the generated script includes code for importing modules
+            from the bundle and for executing one of its modules, very much like
+            "python -m" does. If the bundled modules include exactly one
+            __main__ module, Tsutsumu automatically selects that module. If
+            there are no or several such modules or you want to execute another
+            module, please use the -m/--main option to specify the module name.
 
-            Use the -b/--bundle-only option to omit the bundle runtime and
-            bootstrap code. That way, you can break your application into
-            several bundles.
+            You can use the -b/--bundle-only option to omit the bundle runtime
+            and bootstrap code from the generated script. That way, you can
+            break your application into several bundles. Though you probably
+            want to include that code with your application's primary bundle.
+            The application can then use `Bundle.exec_install()` to load and
+            install such secondary bundles and `Bundle.uninstall()` to uninstall
+            them again.
 
-            By default, the bundle script is written to standard out, which may
-            break the bundle script since Python's standard out is a character
-            instead of a byte stream. Use the -o/--output option to write to a
-            file directly.
+            Tsutsumu always generates the bundle script in binary format.
+            Re-encoding its output or even changing the line endings will likely
+            break the generated script! By default, the script is written to
+            standard out. Please use the -o/--output option to write to a file
+            instead.
+
+            Tsutsumu is © 2023 Robert Grimm. It is licensed under Apache 2.0.
+            The source repository is <https://github.com/apparebit/tsutsumu>
         """),
         formatter_class=width_limited_formatter)
     parser.add_argument(
@@ -56,13 +56,14 @@ if __name__ == '__main__':
         action='store_true',
         help='emit only bundled files and their manifest,\nno runtime code')
     parser.add_argument(
+        '-m', '--main',
+        metavar='MODULE',
+        help="if a package, execute its __main__ module;\n"
+        "if a module, execute this module")
+    parser.add_argument(
         '-o', '--output',
         metavar='FILENAME',
-        help='write bundle script to this file')
-    parser.add_argument(
-        '-p', '--package',
-        metavar='PACKAGE',
-        help="execute this package's __main__ module")
+        help='write bundle to this file')
     parser.add_argument(
         '-r', '--repackage',
         action='store_true',
@@ -75,17 +76,31 @@ if __name__ == '__main__':
         'roots',
         metavar='PKGROOT', nargs='+',
         help="include all Python modules reachable from\nthe package's root directory")
-    options = parser.parse_args(namespace=ToolOptions())
+    return parser
+
+
+@dataclass
+class ToolOptions:
+    bundle_only: bool = False
+    main: 'None | str' = None
+    output: 'None | str' = None
+    repackage: bool = False
+    verbose: bool = False
+    roots: 'list[str]' = field(default_factory=list)
+
+
+def main() -> None:
+    options = parser().parse_args(namespace=ToolOptions())
 
     try:
-        if options.bundle_only and (options.package or options.repackage):
-            raise ValueError('--bundle is incompatible with --package/--repackage')
+        if options.bundle_only and (options.main or options.repackage):
+            raise ValueError('--bundle is incompatible with --main/--repackage')
 
         BundleMaker(
             options.roots,
             bundle_only=options.bundle_only,
+            main=options.main,
             output=options.output,
-            package=options.package,
             repackage=options.repackage,
         ).run()
     except Exception as x:
@@ -94,3 +109,7 @@ if __name__ == '__main__':
         else:
             print(f'Error: {x}')
         sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
