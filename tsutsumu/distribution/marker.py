f72a81b32b09c60e69685e10c6af2b997fa5cfaa
Robert Grimm <apparebit@gmail.com>
2023-07-15 23:31:39 -0400

    Replace ad-hoc helper functions with token string for inputs and token stack for runtime state

--- tsutsumu/distribution/marker.py
+++ tsutsumu/distribution/marker.py
@@ -1,13 +1,12 @@
-from collections.abc import Iterator
 from dataclasses import dataclass
 from enum import auto, Enum
 import re
 from typing import Callable, cast, NoReturn
 
-from .util import canonicalize
+from cargo.name import canonicalize
 
 
-__all__ = ('extract_extra',)
+__all__ = ("extract_extra",)
 
 
 MARKER_SYNTAX = re.compile(
@@ -20,31 +19,45 @@ MARKER_SYNTAX = re.compile(
         | (?P<LIT> '[^']*' | "[^"]*")
         | (?P<VAR>  [a-z] (?: [a-z_]* [a-z])?)
     """,
-    re.VERBOSE)
+    re.VERBOSE,
+)
 
 
-VARIABLE_NAMES = set([
-    'python_version', 'python_full_version', 'os_name', 'sys_platform',
-    'platform_release', 'platform_system', 'platform_version', 'platform_machine',
-    'platform_python_implementation','implementation_name', 'implementation_version',
-    'extra',
-])
+VARIABLE_NAMES = set(
+    [
+        "python_version",
+        "python_full_version",
+        "os_name",
+        "sys_platform",
+        "platform_release",
+        "platform_system",
+        "platform_version",
+        "platform_machine",
+        "platform_python_implementation",
+        "implementation_name",
+        "implementation_version",
+        "extra",
+    ]
+)
 
 
 class TypTok(Enum):
     """A class identifying the type of a token."""
 
-    WS = auto()         # whitespace, dropped early
+    WS = auto()  # whitespace, dropped early
 
-    LIT = auto()        # string literals, incl. their quotes
-    VAR = auto()        # variables incl. extra
-    COMP = auto()       # comparison operators, which combine LIT and VAR
-    BOOL = auto()       # boolean and/or, which combine COMP-triples
-    OPEN = auto()       # open parenthesis
-    CLOSE = auto()      # close parenthesis
+    LIT = auto()  # string literals, incl. their quotes
+    VAR = auto()  # variables incl. extra
+    COMP = auto()  # comparison operators, which combine LIT and VAR
+    BOOL = auto()  # boolean and/or, which combine COMP-triples
+    OPEN = auto()  # open parenthesis
+    CLOSE = auto()  # close parenthesis
 
-    EXTRA = auto()      # an "extra == 'tag'" expression
+    EXTRA = auto()  # an "extra == 'tag'" expression
     NOT_EXTRA = auto()  # any combination of well-formed expressions without extra
+
+
+T = TypTok
 
 
 @dataclass(frozen=True, slots=True)
@@ -54,138 +67,215 @@ class Token:
 
 
 # The canonical not-extra token
-ELIDED = Token(TypTok.NOT_EXTRA, '')
+ELIDED = Token(T.NOT_EXTRA, "")
 
 
 # ======================================================================================
 
+
 def invalid_syntax(*tokens: str | Token) -> NoReturn:
-    fragment = ' '.join(t.content if isinstance(t, Token) else t for t in tokens)
-    raise ValueError(f'invalid syntax in marker "{fragment}"')
+    fragment = "".join(t.content if isinstance(t, Token) else t for t in tokens)
+    raise ValueError(f"invalid syntax in marker '{fragment}'")
 
-def tokenize(marker: str) -> Iterator[Token]:
-    marker = marker.lower()
-    cursor = 0
-    while (t := MARKER_SYNTAX.match(marker, cursor)):
-        cursor = t.end()
-        tag_name = cast(str, t.lastgroup)
-        if tag_name == 'WS':
-            continue
-        tag_content = t.group()
-        if tag_name == 'VAR' and tag_content not in VARIABLE_NAMES:
-            raise ValueError(f'marker contains unknown variable "{tag_content}"')
-        yield Token(TypTok[tag_name], tag_content)
-    if cursor < len(marker):
-        invalid_syntax(marker[cursor:])
 
-def do_apply_comparison(var: Token, op: Token, lit: Token) -> Token:
-    if lit.tag is not TypTok.LIT:
-        invalid_syntax(var, op, lit)
-    if var.content == 'extra':
-        if op.content != '==' or len(lit.content) < 3:
-            invalid_syntax(var, op, lit)
-        return Token(TypTok.EXTRA, canonicalize(lit.content[1:-1]))
+def _do_apply_comparison(left: Token, op: Token, right: Token) -> Token:
+    match left.content, op.content, right.content:
+        case "extra", "==", content if len(content) >= 3:
+            return Token(T.EXTRA, canonicalize(content[1:-1]))
+        case "extra", _, _:
+            invalid_syntax(left, op, right)
+
     return ELIDED
 
-def apply_comparison(left: Token, op: Token, right: Token) -> Token:
-    if op.tag is TypTok.COMP:
-        if left.tag is TypTok.VAR:
-            return do_apply_comparison(left, op, right)
-        elif right.tag is TypTok.VAR:
-            return do_apply_comparison(right, op, left)
-    invalid_syntax(left, op, right)
 
-def apply_junction(left: Token, op: Token, right: Token) -> Token:
-    if op.tag is not TypTok.BOOL:
-        invalid_syntax(left, op, right)
-    if left.tag is TypTok.EXTRA:
-        if right.tag is TypTok.EXTRA and left.content == right.content:
+def apply_operator(left: Token, op: Token, right: Token) -> Token:
+    match left.tag, op.tag, right.tag:
+        case T.VAR, T.COMP, T.LIT:
+            return _do_apply_comparison(left, op, right)
+        case T.LIT, T.COMP, T.VAR:
+            return _do_apply_comparison(right, op, left)
+        case T.EXTRA, T.BOOL, T.EXTRA if left.content == right.content:
             return left
-        if right.tag is TypTok.NOT_EXTRA and op.content == 'and':
+        case T.EXTRA, T.BOOL, T.NOT_EXTRA if op.content == "and":
             return left
-    elif right.tag is TypTok.EXTRA:
-        if left.tag is TypTok.NOT_EXTRA and op.content == 'and':
+        case T.NOT_EXTRA, T.BOOL, T.EXTRA if op.content == "and":
             return right
-    elif left.tag is TypTok.NOT_EXTRA and right.tag is TypTok.NOT_EXTRA:
-        return ELIDED
+        case T.NOT_EXTRA, T.BOOL, T.NOT_EXTRA:
+            return ELIDED
+
     invalid_syntax(left, op, right)
 
-def distill_extra(tokens: list[Token]) -> Token:
-    cursor = 0
-    length = len(tokens)
-    stack: list[Token] = []
 
-    def shift_onto_stack(token: Token) -> None:
-        stack.append(token)
+class TokenString:
+    """
+    A token string. This class represents the input for marker evaluation. It is
+    consumed in strictly linear order from the front, but optimizes the
+    recursive evaluation of (parenthesized) substrings through buffer sharing.
+    """
 
-    def reduce_stack_with(reducer: Callable[[Token, Token, Token], Token]) -> None:
+    @classmethod
+    def parse(cls, marker: str) -> "TokenString":
+        tokens = []
+        cursor = 0
+        while t := MARKER_SYNTAX.match(marker, cursor):
+            cursor = t.end()
+            tag = cast(str, t.lastgroup)
+            content = t.group()
+            if tag == "VAR" and content not in VARIABLE_NAMES:
+                raise ValueError(f"marker contains unknown variable '{content}'")
+            tokens.append(Token(TypTok[tag], content))
+
+        if cursor < len(marker):
+            raise ValueError(f"marker contains invalid syntax '{marker[cursor:]}'")
+
+        return cls(tokens, 0, len(tokens))
+
+    __slots__ = ("_tokens", "_start", "_stop", "_cursor")
+
+    def __init__(self, tokens: list[Token], start: int, stop: int) -> None:
+        assert 0 <= start <= stop <= len(tokens)
+        self._tokens = tokens
+        self._start = start
+        self._stop = stop
+        self._cursor = start
+
+    def __str__(self) -> str:
+        return "".join(t.content for t in self._tokens[self._cursor : self._stop])
+
+    def has_next(self, count: int = 1) -> bool:
+        return self._cursor + count - 1 < self._stop
+
+    def has_triple_with(self, tag: TypTok, content: None | str = None) -> bool:
+        tokens = self._tokens
+        if self._cursor + 2 < self._stop:
+            return False
+        token = tokens[self._cursor + 1]
+        return token.tag is tag and (content is None or content == token.content)
+
+    def peek(self) -> Token:
+        return self._tokens[self._cursor]
+
+    def next(self) -> Token:
+        tokens = self._tokens
+        cursor = self._cursor
+        stop = self._stop
+
+        token = tokens[cursor]
+        while True:
+            cursor += 1
+            if cursor == stop or tokens[cursor].tag is not T.WS:
+                break
+
+        self._cursor = cursor
+        return token
+
+    def parenthesized(self) -> "TokenString":
+        tokens = self._tokens
+        cursor = self._cursor
+        assert tokens[cursor].tag is T.OPEN
+
+        nesting = 0
+        for index in range(cursor + 1, self._stop):
+            token = tokens[index]
+            match token.tag:
+                case T.CLOSE if nesting == 0:
+                    string = TokenString(tokens, cursor + 1, index)
+                    self._cursor = index + 1
+                    return string
+                case T.OPEN:
+                    nesting += 1
+                case T.CLOSE:
+                    nesting -= 1
+
+        raise ValueError(f"opening parenthesis without closing one in '{self}'")
+
+
+class TokenStack:
+    """
+    A token stack. This class captures the incremental execution state during
+    marker evaluation. It draws on familiar parser terminology and techniques
+    because marker evaluation *is* marker parsing.
+    """
+
+    def __init__(self) -> None:
+        self._stack: list[Token] = []
+
+    def __len__(self) -> int:
+        return len(self._stack)
+
+    def stringify(self, count: int) -> str:
+        assert count <= len(self._stack)
+
+        parts = []
+        for index, token in enumerate(self._stack.__reversed__()):
+            if index == count:
+                break
+            parts.append(token.content)
+        return " ".join(parts)
+
+    def unwrap(self) -> Token:
+        assert len(self._stack) == 1
+        return self._stack[0]
+
+    def shift(self, *tokens: Token) -> None:
+        self._stack.extend(tokens)
+
+    def reduce_with(self, reducer: Callable[[Token, Token, Token], Token]) -> None:
+        stack = self._stack
         right = stack.pop()
         op = stack.pop()
         left = stack.pop()
         stack.append(reducer(left, op, right))
 
-    def is_junction_on_stack(content: str, offset: int = 0) -> bool:
-        if len(stack) < offset + 3:
+    def has_triple(self, tag: TypTok, content: None | str = None) -> bool:
+        stack = self._stack
+        if len(stack) < 3:
             return False
-        token = stack[-offset-2]
-        return token.tag is TypTok.BOOL and token.content == content
+        token = stack[-2]
+        if token.tag is not tag or (content is not None and content != token.content):
+            return False
+        token = stack[-1]
+        return token.tag is not T.OPEN
 
-    def peek_token() -> Token:
-        return tokens[cursor]
 
-    def has_tokens(count: int = 1) -> bool:
-        return cursor + count - 1 < length
+def distill_extra(tokens: TokenString) -> Token:
+    stack = TokenStack()
 
-    def next_token() -> Token:
-        nonlocal cursor
-        token = tokens[cursor]
-        cursor += 1
-        return token
-
-    def find_close_token() -> int:
-        nesting = 0
-        for index in range(cursor + 1, length):
-            token = tokens[index]
-            if nesting == 0 and token.tag is TypTok.CLOSE:
-                return index
-            if token.tag is TypTok.OPEN:
-                nesting += 1
-            elif token.tag is TypTok.CLOSE:
-                if nesting == 0:
-                    invalid_syntax(*tokens[index:])
-                nesting -= 1
-        raise ValueError(f'opening parenthesis without closing one')
-
+    # Compute the fixed point of parenthesized, comparsion, and conjunctive
+    # expressions. Once all of them have been reduced, handle disjunctions.
     while True:
-        if has_tokens() and peek_token().tag is TypTok.OPEN:
-            close = find_close_token()
-            shift_onto_stack(distill_extra(tokens[1 : close]))
-            tokens[0 : close + 1] = []
-        elif has_tokens(3):
-            result = apply_comparison(next_token(), next_token(), next_token())
-            shift_onto_stack(result)
-        if is_junction_on_stack('and'):
-            reduce_stack_with(apply_junction)
-        if not has_tokens():
+        if tokens.has_next() and tokens.peek().tag is T.OPEN:
+            parenthesized = tokens.parenthesized()
+            stack.shift(distill_extra(parenthesized))
+        elif tokens.has_next(3):
+            stack.shift(tokens.next(), tokens.next(), tokens.next())
+            if not stack.has_triple(T.COMP):
+                raise ValueError(f"expected comparison, found '{stack.stringify(3)}'")
+            stack.reduce_with(apply_operator)
+        if stack.has_triple(T.BOOL, "and"):
+            stack.reduce_with(apply_operator)
+        if not tokens.has_next():
             break
-        shift_onto_stack(next_token())
+        stack.shift(tokens.next())
 
+    # Reduce disjunctions until the stack has only one token. That's our result.
     while len(stack) > 1:
-        if not is_junction_on_stack('or'):
-            invalid_syntax(*tokens)
-        reduce_stack_with(apply_junction)
+        assert stack.has_triple(T.BOOL, "or")
+        stack.reduce_with(apply_operator)
 
-    return stack[0]
+    return stack.unwrap()
+
 
 def extract_extra(marker: str) -> None | str:
     try:
-        tokens = [*tokenize(marker)]
-        token = distill_extra(tokens)
+        token = distill_extra(TokenString.parse(marker))
     except ValueError:
-        raise ValueError(f'malformed marker "{marker}"')
+        raise ValueError(f"malformed marker '{marker}'")
     else:
-        if token.tag is TypTok.NOT_EXTRA:
-            return None
-        if token.tag is TypTok.EXTRA:
-            return token.content
-        raise ValueError(f'malformed marker "{marker}"')
+        match token.tag:
+            case T.EXTRA:
+                return token.content
+            case T.NOT_EXTRA:
+                return None
+            case _:
+                raise ValueError(f"malformed marker '{marker}'")
