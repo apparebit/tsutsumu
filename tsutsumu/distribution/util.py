61eedb369cc4e114f04d22278df22cee8d345b29
Robert Grimm <apparebit@gmail.com>
2023-07-15 23:30:13 -0400

    Move canonicalize to cargo package

--- tsutsumu/distribution/util.py
+++ tsutsumu/distribution/util.py
@@ -1,13 +1,7 @@
 import datetime
 import re
 
-__all__ = ('canonicalize', 'today_as_version')
-
-_DASHING = re.compile(r'[-_.]+')
-
-def canonicalize(name: str, separator: str = '-') -> str:
-    """Convert a package or tag name to its canonical form."""
-    return _DASHING.sub(separator, name).lower()
+__all__ = ('today_as_version')
 
 def today_as_version() -> str:
     return '.'.join(str(part) for part in datetime.date.today().isocalendar())
