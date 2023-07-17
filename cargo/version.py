ad5f52430b944ac4c635884c1481255d3902091b
Robert Grimm <apparebit@gmail.com>
2023-07-16 21:58:43 -0400

    Clean up Version class and make comparisons work without the need for new sentinel classes

--- cargo/version.py
+++ cargo/version.py
@@ -1,14 +1,14 @@
 import itertools as it
+import math
 import re
-from typing import cast, Literal, NamedTuple, TypeAlias
+from typing import cast, Literal, NamedTuple
 
-from ._infinity import NegativeInfinity, NegativeInfinityType, PositiveInfinity
 
+# mypy: disallow_any_expr = false
 
 __all__ = ("Version",)
 
-
-_VERSION_SYNTAX = re.compile(
+VERSION_SYNTAX = re.compile(
     r"""
         [v]?
         (?:(?P<epoch>[0-9]+)[!])?
@@ -18,6 +18,8 @@ _VERSION_SYNTAX = re.compile(
             (?: [._-]? (?P<pre_number>  [0-9]+) )?
         )?
         (?:
+            (?: [-] (?P<post_simple> [0-9]+) )
+            |
             [._-]?     (?P<post_label>  post|r(?:ev)?)
             (?: [._-]? (?P<post_number> [0-9]+) )?
         )?
@@ -32,215 +34,203 @@ _VERSION_SYNTAX = re.compile(
     re.X | re.I,
 )
 
-
-_RELABEL = {
-    None: None,
+LABELS = {
     "a": "a",
     "alpha": "a",
     "b": "b",
     "beta": "b",
     "c": "rc",
-    "dev": "dev",
-    "post": "post",
     "pre": "rc",
     "preview": "rc",
-    "r": "post",
     "rc": "rc",
+    "post": "post",
+    "r": "post",
     "rev": "post",
+    "dev": "dev",
 }
 
+NOT_A_LABEL = '★'
 
-_LOCAL_SEPARATOR = re.compile(r'[._-]')
-
-
-def _ingest_segment(
-    label: None | str,
-    number: None | str,
-) -> tuple[None | str, None | int]:
-    if label is None:
-        assert number is None
-        return None, None
-    label = _RELABEL[label.lower()]
-    if number is None:
-        return label, 0
-    return label, int(number)
-
-
-def _format_segment(segment: None | int | str) -> str:
-    return "⋯" if segment is None else str(segment)
-
-
-LocalT: TypeAlias = None | tuple[int|str, ...] | NegativeInfinityType
+SEPARATOR = re.compile(r'[._-]')
 
 
 class VersionData(NamedTuple):
     epoch: int
     release: tuple[int, ...]
-    prelabel: None | Literal["", "a", "b", "rc"]
+    status: None | Literal["a", "b", "rc"]
     pre: None | int
     post: None | int
     dev: None | int
-    local: LocalT
+    local: None | str
 
-    def to_key(self) -> 'VersionData':
-        release = tuple(
-            reversed(list(it.dropwhile(lambda v: v == 0, reversed(self.release)))))
+    @classmethod
+    def from_string(cls, version: str) -> 'VersionData':
+        segments = VERSION_SYNTAX.match(version.strip().lower())
+        if segments is None:
+            raise ValueError(f'not a version string "{version}"')
 
-        if self.prelabel is None and self.post is None and self.dev is not None:
-            pre: int = NegativeInfinity
-        elif self.prelabel is None:
-            pre = PositiveInfinity
+        def parse(
+            label: None | str, value: None | str
+        ) -> tuple[None | str, None | int]:
+            return (
+                (None, None)
+                if label is None
+                else (LABELS[label], 0 if value is None else int(value))
+            )
+
+        epoch = cast(int, segments.group("epoch")) or 0
+        release = tuple(int(p) for p in segments.group("release").split("."))
+        status, pre = parse(segments.group('pre_label'), segments.group('pre_number'))
+        if (simple_post := segments.group("post_simple")) is not None:
+            post: None | int = int(simple_post)
         else:
-            pre = cast(int, self.pre)
+            _, post = parse(segments.group("post_label"), segments.group("post_number"))
+        _, dev = parse(segments.group("dev_label"), segments.group("dev_number"))
+        local = segments.group("local")
+
+        status = cast(None | Literal['a', 'b', 'rc'], status)
+        return cls(epoch, release, status, pre, post, dev, local)
+
+    def release_components(self, precision: int) -> tuple[int, ...]:
+        return tuple(it.islice(it.chain(self.release, it.repeat(0)), precision))
+
+    def release_text(self) -> str:
+        return '.'.join(str(n) for n in self.release)
+
+    def to_key(
+        self,
+    ) -> tuple[
+        int, tuple[int, ...], str, float, int, float, tuple[tuple[int, str], ...]
+    ]:
+        release = tuple(
+            reversed(list(it.dropwhile(lambda v: v == 0, reversed(self.release))))
+        )
+
+        if self.pre is None and self.post is None and self.dev is not None:
+            pre: float = -1
+        elif self.pre is None:
+            pre = math.inf
+        else:
+            pre = self.pre
 
         if self.post is None:
-            post: int = NegativeInfinity
+            post: int = -1
         else:
             post = self.post
 
         if self.dev is None:
-            dev: int = PositiveInfinity
+            dev: float = math.inf
         else:
             dev = self.dev
 
         if self.local is None:
-            local: LocalT = NegativeInfinity
+            local: tuple[tuple[int, str], ...] = ()
         else:
             local = tuple(
-                (i, '') if isinstance(i, int) else (NegativeInfinity, i)
-                for i in self.local
+                (i, '') if isinstance(i, int) else (-1, i)
+                for i in SEPARATOR.split(self.local)
             )
 
-        return VersionData(self.epoch, release, '', pre, post, dev, local)
+        return self.epoch, release, self.status or "★", pre, post, dev, local
 
 
 class Version:
+    __slots__ = ('_version', '_key', '_text')
 
-    __slots__ = ("_ver", "_key")
-
-    @classmethod
-    def of(cls, text: str) -> "Version":
-        parts = _VERSION_SYNTAX.match(text.strip())
-        if parts is None:
-            raise ValueError(f'not a marker "{text}"')
-
-        epoch = cast(int, parts.group("epoch") or 0)
-        release = tuple(int(p) for p in parts.group("release").split("."))
-        rawprelabel, pre = _ingest_segment(
-            parts.group("pre_label"), parts.group("pre_number")
-        )
-        prelabel = cast(None | Literal['a', 'b', 'rc'], rawprelabel)
-        post = _ingest_segment(parts.group("post_label"), parts.group("post_number"))[1]
-        dev = _ingest_segment(parts.group("dev_label"), parts.group("dev_number"))[1]
-        local = None
-        rawlocal = parts.group("local")
-        if rawlocal is not None:
-            local = tuple(
-                p.lower() if not p.isdigit() else int(p)
-                for p in _LOCAL_SEPARATOR.split(rawlocal)
-            )
-        return cls(epoch, release, prelabel, pre, post, dev, local)
-
-    def __init__(
-        self,
-        epoch: int,
-        release: tuple[int, ...],
-        prelabel: None | Literal["a", "b", "rc"],
-        pre: None | int,
-        post: None | int,
-        dev: None | int,
-        local: None | tuple[int|str, ...],
-    ) -> None:
-        self._ver = VersionData(epoch, release, prelabel, pre, post, dev, local)
-        self._key = self._ver.to_key()
-
-    @property
-    def astuple(self) -> VersionData:
-        return self._ver
+    def __init__(self, version: str) -> None:
+        self._version = VersionData.from_string(version)
+        self._key = self._version.to_key()
+        self._text: None | str = None
 
     @property
     def epoch(self) -> int:
-        return self._ver[0]
+        return self._version.epoch
 
     @property
     def release(self) -> tuple[int, ...]:
-        return self._ver[1]
-
-    @property
-    def pre(self) -> None | tuple[Literal['', 'a', 'b', 'rc'], int]:
-        prelabel = self._ver[2]
-        if prelabel is None:
-            return None
-        return prelabel, cast(int, self._ver[3])
-
-    @property
-    def post(self) -> None | int:
-        return self._ver[4]
-
-    @property
-    def dev(self) -> None | int:
-        return self._ver[5]
-
-    @property
-    def local(self) -> None | str:
-        return self._ver[6]
+        return self._version.release
 
     @property
     def is_prerelease(self) -> bool:
-        return self.pre is not None or self.dev is not None
+        return self._version.status is not None
+
+    @property
+    def status(self) -> None | Literal['a', 'b', 'rc']:
+        return self._version.status
+
+    @property
+    def pre(self) -> None | int:
+        return self._version.pre
 
     @property
     def is_postrelease(self) -> bool:
-        return self.post is not None
+        return self._version.post is not None
+
+    @property
+    def post(self) -> None | int:
+        return self._version.post
 
     @property
     def is_devrelease(self) -> bool:
         return self.dev is not None
 
-    def __str__(self) -> str:
-        fragments = []
-        if (epoch := self.epoch) != 0:
-            fragments.append(f"{epoch}!")
-        fragments.append(".".join(str(num) for num in self.release))
-        if (pre := self.pre) is not None:
-            fragments.append(f".{pre[0]}{pre[1]}")
-        if (post := self.post) is not None:
-            fragments.append(f".post{post}")
-        if (dev := self.dev) is not None:
-            fragments.append(f".dev{dev}")
-        if (local := self.local) is not None:
-            fragments.append(f"+{local}")
-        return "".join(fragments)
+    @property
+    def dev(self) -> None | int:
+        return self._version.dev
 
-    def __repr__(self) -> str:
-        release = ".".join(str(num) for num in self.release)
-        pre = "⋯, ⋯" if (prep := self.pre) is None else f"{prep[0]}, {prep[1]}"
-        post = _format_segment(self.post)
-        dev = _format_segment(self.dev)
-        local = _format_segment(self.local)
-        return f"Version({self.epoch}, {release}, {pre}, {post}, {dev}, {local})"
+    @property
+    def local(self) -> None | str:
+        return self._version.local
+
+    def astuple(self) -> VersionData:
+        return self._version
 
     def __lt__(self, other: object) -> bool:
-        if not isinstance(other, Version):
-            return NotImplemented
-        return self._key < other._key
+        if isinstance(other, Version):
+            return self._key < other._key
+        return NotImplemented
 
     def __le__(self, other: object) -> bool:
-        if not isinstance(other, Version):
-            return NotImplemented
-        return self._key <= other._key
+        if isinstance(other, Version):
+            return self._key <= other._key
+        return NotImplemented
 
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, Version):
-            return NotImplemented
-        return self._key == other._key
+        if isinstance(other, Version):
+            return self._key == other._key
+        return NotImplemented
 
     def __ge__(self, other: object) -> bool:
-        if not isinstance(other, Version):
-            return NotImplemented
-        return self._key >= other._key
+        if isinstance(other, Version):
+            return self._key >= other._key
+        return NotImplemented
 
     def __gt__(self, other: object) -> bool:
-        if not isinstance(other, Version):
-            return NotImplemented
-        return self._key > other._key
+        if isinstance(other, Version):
+            return self._key > other._key
+        return NotImplemented
+
+    def __str__(self) -> str:
+        if self._text is not None:
+            return self._text
+
+        data = self._version
+        fragments = []
+
+        if (epoch := data.epoch) > 0:
+            fragments.append(f'{epoch}!')
+        fragments.append(data.release_text())
+        if (status := data.status) is not None:
+            fragments.append(f'{status}{data.pre}')
+        if (post := data.post) is not None:
+            fragments.append(f'.post{post}')
+        if (dev := data.dev) is not None:
+            fragments.append(f'.dev{dev}')
+        if (local := data.local) is not None:
+            fragments.append(f'+{local}')
+
+        self._text = text = ''.join(fragments)
+        return text
+
+    def __repr__(self) -> str:
+        return f"Version({', '.join(f'{f!r}' for f in self._version)})"
