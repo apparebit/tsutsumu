b7816991c282e0ec91d0f9886fc5fee020a2f854
Robert Grimm <apparebit@gmail.com>
2023-07-16 22:00:21 -0400

    Add version comparison tests, support for logging

--- test/cargo_extra.py
+++ test/cargo_extra.py
@@ -1,16 +1,18 @@
+from typing import Callable
 from tsutsumu.distribution.requirement import parse_requirement
 
-for requirement, expected_result in (
-    ('spam', ('spam', [], [], None)),
-    ('spam [ can ,label]', ('spam', ['can', 'label'], [], None)),
-    ('spam >6.6.5, < 6.6.6', ('spam', [], ['>6.6.5', '<6.6.6'], None)),
-    ('spam ; extra == "can"', ('spam', [], [], 'can')),
-    ('spam; "can"==  extra', ('spam', [], [], 'can')),
-    ('spam[bacon](==2.0)', ('spam', ['bacon'], ['==2.0'], None)),
-    ('spam; os_name != "bacon" and os_name != "ham" and extra == "tofu"',
-        ('spam', [], [], 'tofu')),
-    ('spam; extra == "bacon" or "bacon" == extra', ('spam', [], [], 'bacon')),
-):
-    requirement_quadruple = parse_requirement(requirement)
-    print(f'    {requirement_quadruple}')
-    assert requirement_quadruple == expected_result
+def test_parse_requirement(logger: Callable[[str], None]) -> None:
+    for requirement, expected_result in (
+        ('spam', ('spam', [], [], None)),
+        ('spam [ can ,label]', ('spam', ['can', 'label'], [], None)),
+        ('spam >6.6.5, < 6.6.6', ('spam', [], ['>6.6.5', '<6.6.6'], None)),
+        ('spam ; extra == "can"', ('spam', [], [], 'can')),
+        ('spam; "can"==  extra', ('spam', [], [], 'can')),
+        ('spam[bacon](==2.0)', ('spam', ['bacon'], ['==2.0'], None)),
+        ('spam; os_name != "bacon" and os_name != "ham" and extra == "tofu"',
+            ('spam', [], [], 'tofu')),
+        ('spam; extra == "bacon" or "bacon" == extra', ('spam', [], [], 'bacon')),
+    ):
+        requirement_quadruple = parse_requirement(requirement)
+        logger(f'{requirement_quadruple}')
+        assert requirement_quadruple == expected_result
--- test/cargo_version.py
+++ test/cargo_version.py
@@ -1,6 +1,11 @@
+from operator import lt, le, eq, ne, ge, gt
+from typing import Callable
+
 from cargo.version import Version
 
-def test_parse_version() -> None:
+OPERATOR_NAMES = {'lt':'<', 'le':'<=', 'eq':'==', 'ne':'!=', 'ge':'>=', 'gt':'>'}
+
+def test_parse_version(logger: Callable[[str], None]) -> None:
     for input, expected in (
         ('1.2.3', (0, (1, 2, 3), None, None, None, None, None)),
         ('1.2.3a', (0, (1, 2, 3), 'a', 0, None, None, None)),
@@ -19,8 +24,31 @@ def test_parse_version() -> None:
         ('v1.2.3.dev1', (0, (1, 2, 3), None, None, None, 1, None)),
         ('v1.2.3dev2', (0, (1, 2, 3), None, None, None, 2, None)),
         ('v1.2.3.pre-4.post-5.dev-6', (0, (1, 2, 3), 'rc', 4, 5, 6, None)),
-        ('1.2.3+local', (0, (1, 2, 3), None, None, None, None, ('local',))),
+        ('1.2.3+local', (0, (1, 2, 3), None, None, None, None, 'local')),
     ):
-        actual = Version.of(input)
-        print(f'    {actual!s:>20} --- {actual!r}')
-        assert actual.astuple == expected
+        actual = Version(input)
+        logger(f'{actual!s:>20} --- {actual!r}')
+        assert actual.astuple() == expected
+
+COMPARISONS = (
+    ('1.2.3', eq, '1.2.3'),
+    ('1.2.3', eq, '1.2.3.0.0'),
+    ('1.2.3', ne, '1.2.2'),
+    ('1.2.3a', lt, '1.2.3'),
+    ('1.2.3dev', lt, '1.2.3'),
+    ('1.2.3pre.dev', lt, '1.2.3pre'),
+    ('1.2.3.post.dev', lt, '1.2.3.post'),
+    ('1.2.3pre', lt, '1.2.3.post'),
+)
+
+def test_compare_versions(logger: Callable[[str], None]) -> None:
+    for s1, op, s2 in COMPARISONS:
+        v1, cp, v2 = Version(s1), OPERATOR_NAMES[op.__name__], Version(s2)
+        logger(f'{str(v1):>16s} {cp:<2} {v2}')
+        assert op(v1, v2)
+        if cp in ('==', '!='):
+            logger(f'{str(v2):>16s} {cp} {v1}')
+            assert op(v2, v1)
+        elif cp == '<':
+            logger(f'{str(v2):>16s} >  {v1}')
+            assert gt(v2, v1)
