b6011ed4e7902507a693da5fd95e9f65128aedd1
Robert Grimm <apparebit@gmail.com>
2023-07-20 14:15:55 -0400

    Update tests and add more tests

--- test/cargo_extra.py
+++ test/cargo_extra.py
@@ -1,18 +1,32 @@
-from typing import Callable
-from tsutsumu.distribution.requirement import parse_requirement
+from .console import Console
+from cargo.marker import extract_extra
+from cargo.requirement import Requirement
 
-def test_parse_requirement(logger: Callable[[str], None]) -> None:
-    for requirement, expected_result in (
-        ('spam', ('spam', [], [], None)),
-        ('spam [ can ,label]', ('spam', ['can', 'label'], [], None)),
-        ('spam >6.6.5, < 6.6.6', ('spam', [], ['>6.6.5', '<6.6.6'], None)),
-        ('spam ; extra == "can"', ('spam', [], [], 'can')),
-        ('spam; "can"==  extra', ('spam', [], [], 'can')),
-        ('spam[bacon](==2.0)', ('spam', ['bacon'], ['==2.0'], None)),
+def test_parse_requirement(console: Console) -> None:
+    for input, expected_result in (
+        ('spam', ('spam', (), (), None)),
+        ('spam [ can ,label]', ('spam', ('can', 'label'), (), None)),
+        ('spam >6.6.5, < 6.6.6', ('spam', (), ('>6.6.5', '<6.6.6'), None)),
+        ('spam ; extra == "can"', ('spam', (), (), 'can')),
+        ('spam; "can"==  extra', ('spam', (), (), 'can')),
+        ('spam[bacon](==2.0)', ('spam', ('bacon',), ('==2.0',), None)),
         ('spam; os_name != "bacon" and os_name != "ham" and extra == "tofu"',
-            ('spam', [], [], 'tofu')),
-        ('spam; extra == "bacon" or "bacon" == extra', ('spam', [], [], 'bacon')),
+            ('spam', (), (), 'tofu')),
+        ('spam; extra == "bacon" or "bacon" == extra', ('spam', (), (), 'bacon')),
     ):
-        requirement_quadruple = parse_requirement(requirement)
-        logger(f'{requirement_quadruple}')
-        assert requirement_quadruple == expected_result
+        output = Requirement.from_string(input)
+        # The conversion to a plain tuple isn't necessary for correctness, but
+        # it helps reduce the clutter in verbose mode.
+        console.assert_eq(tuple(output), expected_result)
+
+def test_extract_extra(console: Console) -> None:
+    for marker in (
+        'os_name != "a" and os_name != "b"',
+        '(os_name != "a") and os_name != "b"',
+        'os_name != "a" and (os_name != "b")',
+        '(os_name != "a") and (os_name != "b")',
+        '(((os_name != "a"))) and os_name != "b"',
+        '(os_name != "a" and (os_name != "b"))',
+
+    ):
+        console.assert_eq(extract_extra(marker), None)
--- test/cargo_version.py
+++ test/cargo_version.py
@@ -1,54 +1,71 @@
-from operator import lt, le, eq, ne, ge, gt
-from typing import Callable
+from .console import Console
+from cargo.version import Version, Specifier
 
-from cargo.version import Version
 
-OPERATOR_NAMES = {'lt':'<', 'le':'<=', 'eq':'==', 'ne':'!=', 'ge':'>=', 'gt':'>'}
-
-def test_parse_version(logger: Callable[[str], None]) -> None:
-    for input, expected in (
-        ('1.2.3', (0, (1, 2, 3), None, None, None, None, None)),
-        ('1.2.3a', (0, (1, 2, 3), 'a', 0, None, None, None)),
-        ('1.2.3b1', (0, (1, 2, 3), 'b', 1, None, None, None)),
-        ('1.2.3.b2', (0, (1, 2, 3), 'b', 2, None, None, None)),
-        ('1.2.3.preview-3', (0, (1, 2, 3), 'rc', 3, None, None, None)),
-        ('1.2.3.rc.4', (0, (1, 2, 3), 'rc', 4, None, None, None)),
-        ('1.2.3.rc_5', (0, (1, 2, 3), 'rc', 5, None, None, None)),
-        ('1.2.3post', (0, (1, 2, 3), None, None, 0, None, None)),
-        ('1.2.3.r1', (0, (1, 2, 3), None, None, 1, None, None)),
-        ('1.2.3.rev2', (0, (1, 2, 3), None, None, 2, None, None)),
-        ('1.2.3.post_3', (0, (1, 2, 3), None, None, 3, None, None)),
-        ('1.2.3.post.4', (0, (1, 2, 3), None, None, 4, None, None)),
-        ('1.2.3.post-5', (0, (1, 2, 3), None, None, 5, None, None)),
-        ('v1.2.3.dev', (0, (1, 2, 3), None, None, None, 0, None)),
-        ('v1.2.3.dev1', (0, (1, 2, 3), None, None, None, 1, None)),
-        ('v1.2.3dev2', (0, (1, 2, 3), None, None, None, 2, None)),
-        ('v1.2.3.pre-4.post-5.dev-6', (0, (1, 2, 3), 'rc', 4, 5, 6, None)),
-        ('1.2.3+local', (0, (1, 2, 3), None, None, None, None, 'local')),
+def test_parse_version(console: Console) -> None:
+    for input, output_text, output_data in (
+        ('1.2.3', '1.2.3', (0, (1, 2, 3), None, None, None, None, None)),
+        ('1.2.3a', '1.2.3a0', (0, (1, 2, 3), 'a', 0, None, None, None)),
+        ('1.2.3b1', '1.2.3b1', (0, (1, 2, 3), 'b', 1, None, None, None)),
+        ('1.2.3.b2', '1.2.3b2', (0, (1, 2, 3), 'b', 2, None, None, None)),
+        ('1.2.3.preview-3', '1.2.3rc3', (0, (1, 2, 3), 'rc', 3, None, None, None)),
+        ('1.2.3.rc.4', '1.2.3rc4', (0, (1, 2, 3), 'rc', 4, None, None, None)),
+        ('1.2.3.rc_5', '1.2.3rc5', (0, (1, 2, 3), 'rc', 5, None, None, None)),
+        ('1.2.3post', '1.2.3.post0', (0, (1, 2, 3), None, None, 0, None, None)),
+        ('1.2.3.r1', '1.2.3.post1', (0, (1, 2, 3), None, None, 1, None, None)),
+        ('1.2.3.rev2', '1.2.3.post2', (0, (1, 2, 3), None, None, 2, None, None)),
+        ('1.2.3.post_3', '1.2.3.post3', (0, (1, 2, 3), None, None, 3, None, None)),
+        ('1.2.3.post.4', '1.2.3.post4', (0, (1, 2, 3), None, None, 4, None, None)),
+        ('1.2.3.post-5', '1.2.3.post5', (0, (1, 2, 3), None, None, 5, None, None)),
+        ('v1.2.3.dev', '1.2.3.dev0', (0, (1, 2, 3), None, None, None, 0, None)),
+        ('v1.2.3.dev1', '1.2.3.dev1', (0, (1, 2, 3), None, None, None, 1, None)),
+        ('v1.2.3dev2', '1.2.3.dev2', (0, (1, 2, 3), None, None, None, 2, None)),
+        ('v1.2.3.pre-4.post-5.dev-6',
+            '1.2.3rc4.post5.dev6', (0, (1, 2, 3), 'rc', 4, 5, 6, None)),
+        ('1.2.3+local', '1.2.3+local', (0, (1, 2, 3), None, None, None, None, 'local')),
     ):
         actual = Version(input)
-        logger(f'{actual!s:>20} --- {actual!r}')
-        assert actual.astuple() == expected
+        console.assert_eq(str(actual), output_text)
+        console.assert_eq(actual.data, output_data)
 
-COMPARISONS = (
-    ('1.2.3', eq, '1.2.3'),
-    ('1.2.3', eq, '1.2.3.0.0'),
-    ('1.2.3', ne, '1.2.2'),
-    ('1.2.3a', lt, '1.2.3'),
-    ('1.2.3dev', lt, '1.2.3'),
-    ('1.2.3pre.dev', lt, '1.2.3pre'),
-    ('1.2.3.post.dev', lt, '1.2.3.post'),
-    ('1.2.3pre', lt, '1.2.3.post'),
-)
 
-def test_compare_versions(logger: Callable[[str], None]) -> None:
-    for s1, op, s2 in COMPARISONS:
-        v1, cp, v2 = Version(s1), OPERATOR_NAMES[op.__name__], Version(s2)
-        logger(f'{str(v1):>16s} {cp:<2} {v2}')
-        assert op(v1, v2)
-        if cp in ('==', '!='):
-            logger(f'{str(v2):>16s} {cp} {v1}')
-            assert op(v2, v1)
-        elif cp == '<':
-            logger(f'{str(v2):>16s} >  {v1}')
-            assert gt(v2, v1)
+def test_compare_versions(console: Console) -> None:
+    for s1, op, s2 in (
+        ('1.2.3', 'eq', '1.2.3'),
+        ('1.2.3', 'eq', '1.2.3.0.0'),
+        ('1.2.3', 'ne', '1.2.2'),
+        ('1.2.3a', 'lt', '1.2.3'),
+        ('1.2.3dev', 'lt', '1.2.3'),
+        ('1.2.3pre.dev', 'lt', '1.2.3pre'),
+        ('1.2.3.post.dev', 'lt', '1.2.3.post'),
+        ('1.2.3pre', 'lt', '1.2.3.post'),
+    ):
+        v1, v2 = Version(s1), Version(s2)
+        console.assert_op(op, v1, v2)
+        if op in ('eq', 'ne'):
+            console.assert_op(op, v2, v1)
+        elif op == 'lt':
+            console.assert_op('gt', v2, v1)
+
+
+def test_apply_specifier(console: Console) -> None:
+    for spec_text, version_text, result in (
+        ('> 1.2.3', '1.2.3post', False),
+        ('> 1.2.3', '1.2.4', True),
+        ('>= 1.2.3', '1.2.3', True),
+        ('>= 1.2.3', '1.2.4', True),
+        ('== 1.2.3', '1.2.4', False),
+        ('== 1.2.3', '1.2.3.0', True),
+        ('== 1.2.3.0', '1.2.3', True),
+        ('== 1.2.*', '1.2', True),
+        ('== 1.2.*', '1.2.3', True),
+        ('== 1.2.*', '1.2.4.rc5', True),
+        ('== 1.2.*', '1.2.4.post5', True),
+        ('~= 1.2.3', '1.2.2', False),
+        ('~= 1.2.3', '1.2.3', True),
+        ('~= 1.2.3', '1.2.9999999999999999999', True),
+        ('~= 1.2.3', '1.3.0', False),
+    ):
+        spec = Specifier(spec_text)
+        version = Version(version_text)
+        console.assert_op(spec, version, expected=result)
